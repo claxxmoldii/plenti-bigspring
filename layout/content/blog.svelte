c97fcce1476020011607d763ba74ff5544f758e8
jimafisk <jimafisk@gmail.com>
2021-02-04 12:28:38 -0500

    Add blog + style, fix nav links, add entrypoint to head.

---
+++ layout/content/blog.svelte
@@ -0,0 +1,8 @@
+<h1>This will become the blog landing page</h1>
+
+<style>
+  a, button, select {
+    cursor: pointer;
+    transition: .2s ease;
+  }
+</style>
--- layout/global/head.svelte
+++ layout/global/head.svelte
@@ -8,6 +8,7 @@
 
   <title>{ title }</title>
 
+  <script type="module" src="/spa/ejected/main.js"></script>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
   <link rel="icon" type="image/svg+xml" href="/assets/favicon.svg">
   <link rel='stylesheet' href='/spa/bundle.css'>
